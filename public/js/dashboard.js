1507ee35733b69341e8a718b963dd4d3ce9f2214
Marvin E. Pratama <marvin@post.com>
2023-08-22 15:35:04 +0700

    fix's fetchData

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,133 +3,81 @@ document.addEventListener("DOMContentLoaded", async () => {
     'script[src="/js/dashboard.js"]',
   );
   const apiKey = scriptElement.getAttribute("sct");
-  const table1 = document.getElementById("copy");
-  const table2 = document.getElementById("update");
-  const table3 = document.getElementById("analytics");
 
-  fetch("/api/v1/shorten", {
-    headers: {
-      "Content-Type": "application/json",
-      "API-Key": apiKey,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((rowData) => {
-        const row = document.createElement("tr");
+  const tables = {
+    copy: document.getElementById("copy"),
+    update: document.getElementById("update"),
+    analytics: document.getElementById("analytics"),
+  };
 
-        let idCounter = 1;
-
-        const idCell = document.createElement("td");
-        idCell.textContent = idCounter++;
-        row.appendChild(idCell);
-
-        const shortLinkCell = document.createElement("td");
-        shortLinkCell.textContent = rowData.uniqueUrl;
-        row.appendChild(shortLinkCell);
-
-        const originalLinkCell = document.createElement("td");
-        originalLinkCell.textContent = rowData.originalUrl;
-        row.appendChild(originalLinkCell);
-
-        const actionCell = document.createElement("td");
-        const copyButton = document.createElement("button");
-        copyButton.className = "btn btn-sm btn-primary";
-        copyButton.textContent = "Copy";
-        copyButton.addEventListener("click", () => {
-          copyToClipboard(rowData.shortUrl);
-        });
-        actionCell.appendChild(copyButton);
-        row.appendChild(actionCell);
-
-        table1.appendChild(row);
-      });
+  async function fetchData() {
+    const response = await fetch("/api/v1/shorten", {
+      headers: {
+        "Content-Type": "application/json",
+        "API-Key": apiKey,
+      },
     });
+    const data = await response.json();
+    return data;
+  }
 
-  fetch("/api/v1/shorten", {
-    headers: {
-      "Content-Type": "application/json",
-      "API-Key": apiKey,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((rowData) => {
-        const row = document.createElement("tr");
+  async function populateTable(table, buttonClass, buttonText, actionCallback) {
+    const data = await fetchData();
+    let idCounter = 1;
 
-        let idCounter = 1;
+    data.forEach((rowData) => {
+      const row = document.createElement("tr");
 
-        const idCell = document.createElement("td");
-        idCell.textContent = idCounter++;
-        row.appendChild(idCell);
+      const idCell = document.createElement("td");
+      idCell.textContent = idCounter++;
+      row.appendChild(idCell);
 
-        const shortLinkCell = document.createElement("td");
-        shortLinkCell.textContent = rowData.uniqueUrl;
-        row.appendChild(shortLinkCell);
+      const shortLinkCell = document.createElement("td");
+      shortLinkCell.textContent = rowData.uniqueUrl;
+      row.appendChild(shortLinkCell);
 
-        const originalLinkCell = document.createElement("td");
-        originalLinkCell.textContent = rowData.originalUrl;
-        row.appendChild(originalLinkCell);
+      const originalLinkCell = document.createElement("td");
+      originalLinkCell.textContent = rowData.originalUrl;
+      row.appendChild(originalLinkCell);
 
-        const actionCell = document.createElement("td");
-        const updateButton = document.createElement("button");
-        updateButton.className = "btn btn-sm btn-info";
-        updateButton.textContent = "Update";
-        updateButton.addEventListener("click", () => {
-          navigateToUpdatePage(rowData.id);
-        });
-        actionCell.appendChild(updateButton);
-        row.appendChild(actionCell);
-
-        table2.appendChild(row);
-      });
-    });
-
-  fetch("/api/v1/shorten", {
-    headers: {
-      "Content-Type": "application/json",
-      "API-Key": apiKey,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((rowData) => {
-        const row = document.createElement("tr");
-
-        let idCounter = 1;
-
-        const idCell = document.createElement("td");
-        idCell.textContent = idCounter++;
-        row.appendChild(idCell);
-
-        const shortLinkCell = document.createElement("td");
-        shortLinkCell.textContent = rowData.uniqueUrl;
-        row.appendChild(shortLinkCell);
-
-        const originalLinkCell = document.createElement("td");
-        originalLinkCell.textContent = rowData.originalUrl;
-        row.appendChild(originalLinkCell);
-
+      if (table === tables.analytics) {
         const clickCountLinkCell = document.createElement("td");
         const clickCountBadge = document.createElement("span");
         clickCountBadge.className = "badge";
         clickCountBadge.textContent = "Clicks " + rowData.clickCount;
         clickCountLinkCell.appendChild(clickCountBadge);
         row.appendChild(clickCountLinkCell);
+      }
 
-        const actionCell = document.createElement("td");
-        const deleteButton = document.createElement("button");
-        deleteButton.className = "btn btn-sm btn-warning";
-        deleteButton.textContent = "Delete";
-        deleteButton.addEventListener("click", () => {
-          navigateToDeletePage(rowData.id);
-        });
-        actionCell.appendChild(deleteButton);
-        row.appendChild(actionCell);
-
-        table3.appendChild(row);
+      const actionCell = document.createElement("td");
+      const actionButton = document.createElement("button");
+      actionButton.className = buttonClass;
+      actionButton.textContent = buttonText;
+      actionButton.addEventListener("click", () => {
+        if (table === tables.copy) {
+          actionCallback(rowData.shortUrl);
+        } else {
+          actionCallback(rowData.id);
+        }
       });
+      actionCell.appendChild(actionButton);
+      row.appendChild(actionCell);
+
+      table.appendChild(row);
     });
+  }
+
+  populateTable(tables.copy, "btn btn-sm btn-primary", "Copy", (shortUrl) => {
+    copyToClipboard(shortUrl);
+  });
+
+  populateTable(tables.update, "btn btn-sm btn-info", "Update", (id) => {
+    navigateToUpdatePage(id);
+  });
+
+  populateTable(tables.analytics, "btn btn-sm btn-warning", "Delete", (id) => {
+    navigateToDeletePage(id);
+  });
 });
 
 function copyToClipboard(text) {
